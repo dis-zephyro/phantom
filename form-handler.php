13b31b2a205a5726a66457630b81722f636617ee
dis-zephyro <zephyro@yandex.ru>
2015-12-09 23:36:40 +0300

    v1

---
+++ form-handler.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $form = $_POST['form'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+
+    require 'class.phpmailer.php';
+    $thm = 'Вам отправлено сообщение с сайта a-mebel - ' . $form;
+    $msg = "<strong>Форма:</strong> $form <br/>";
+    if(isset($name)){$msg .= "<strong>Имя:</strong> $name <br/>";}
+    if(isset($phone)){$msg .= "<strong>Телефон:</strong> $phone <br/>";}
+    if(isset($email)){$msg .= "<strong>Email:</strong> $email <br/>";}
+    if(isset($message)){$msg .= "<strong>Вопрос:</strong> $message <br/>";}
+
+
+    $mail = new PHPMailer();
+    $mail->From = 'noreply@'. $_SERVER['SERVER_NAME']; // от кого
+    $mail->FromName = $_SERVER['SERVER_NAME']; // от кого
+    $mail->AddAddress('fantomm59@yandex.ru', ''); // кому - адрес, Имя
+
+    $mail->IsHTML(true); // выставляем формат письма HTML
+    $mail->Subject = $thm; // тема письма
+    $mail->Body = $msg;
+
+    if (!$mail->Send()) die('Mailer Error: ' . $mail->ErrorInfo);
+
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+}
+?>
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+$('.topnav li a').click(function(){
+    var str=$(this).attr('href');
+    $.scrollTo(str, 500, {offset:0 });
+    return false;
+});
+
+$('.btn-up').click(function(){
+    var str=$(this).attr('href');
+    $.scrollTo(str, 500, {offset:0 });
+    return false;
+});
+
+
 $('.showcase-more span').click(function(){
     $('.showcase-hidden').show('fast');
     $('.showcase-more').hide();
@@ -6,4 +19,90 @@ $('.showcase-more span').click(function(){
 $('.showcase-hide span').click(function(){
     $('.showcase-hidden').hide('fast');
     $('.showcase-more').show();
+});
+
+// ----- Маска ----------
+
+jQuery(function($){
+    $("input[name='phone']").mask("+7(999) 999-9999");
+});
+
+
+//  Modal
+
+$(".btn-modal").fancybox({
+    'padding'    : 0,
+    'tpl'        : {
+        closeBtn : '<a title="Close" class="btn_close" href="javascript:;"></a>'
+    }
+});
+
+
+$(".btn-detail").fancybox({
+    'max-height'	 : '50%',
+    'height'	 : '50%',
+    'padding'    : 0,
+    'tpl'        : {
+        closeBtn : '<a title="Close" class="btn_close" href="javascript:;"></a>'
+    }
+});
+
+// Map
+
+ymaps.ready(init);
+
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [57.9947,55.9464],
+        zoom: 15,
+        controls: ['smallMapDefaultSet']
+    });
+
+    myPlacemark = new ymaps.Placemark([57.9947,55.9464], {
+        hintContent: ''
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/placemark.png',
+        iconImageSize: [47, 69],
+        iconImageOffset: [-23, -70]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('multiTouch');
+    myMap.geoObjects.add(myPlacemark);
+}
+
+
+
+$(document).ready(function() {
+
+    $('.btn-submit').click(function() {
+
+        $('body').find('form:not(this)').children('div').removeClass('red'); //удаление всех сообщение об ошибке(валидатора)
+        var answer = checkForm($(this).closest('form').get(0)); //ответ от валидатора
+        if(answer != false)
+        {
+            var $form = $(this).closest('form'),
+                name    =     $('input[name="name"]', $form).val(),
+                type    =     $('input[name="type"]', $form).val(),
+                phone   =     $('input[name="phone"]', $form).val(),
+                email   =     $('input[name="email"]', $form).val(),
+                form    =     $('input[name="form"]', $form).val(),
+                message =     $('textarea[name="message"]', $form).val();
+            console.log(name, phone, email, form, type, message);
+            $.ajax({
+                type: "POST",
+                url: "form-handler.php",
+                data: {name: name, phone: phone, email:email, form:form, type:type, message:message}
+            }).done(function(msg) {
+                $('form').find('input[type=text], textarea').val('');
+                console.log('удачно');
+                document.location.href = "http://fantom159.ru/done.html";
+            });
+        }
+    });
+
 });
